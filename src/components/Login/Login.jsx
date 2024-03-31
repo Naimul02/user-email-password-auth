52b5a090cfbfb798957ffa2226eb59f0233eb9ec
Naimul02 <naimulislum39@gmail.com>
2024-03-31 23:43:25 +0600

    commit no 6

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,116 @@
-
+import {
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import auth from "../../firebase/firebase.config";
 
 const Login = () => {
+  const [registerError, setRegisterError] = useState("");
+  const [success, setSuccess] = useState("");
+  const emailRef = useRef(null);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    // reset error and success
+    setRegisterError("");
+    setSuccess("");
+
+    // add validation
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+        setSuccess("User Logged in Successfully.");
+      })
+      .catch((error) => {
+        console.error(error);
+        setRegisterError(error.message);
+      });
+  };
+
+  const handleForgetPassword = () => {
+    const email = emailRef.current.value;
+    if (!email) {
+      console.log("hello mama");
+      return;
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      console.log("please write a valid email");
+      return;
+    }
+    // send validationemail
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("please check your email");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
   return (
-    <div>
-        <h3 className="text-3xl">Please Login</h3>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body" onSubmit={handleLogin}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                ref={emailRef}
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a
+                  href="#"
+                  className="label-text-alt link link-hover"
+                  onClick={handleForgetPassword}
+                >
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+
+            {registerError && <p className="text-red-700">{registerError}</p>}
+            {success && <p className="text-green-600">{success}</p>}
+            <p>
+              New to this website Please{" "}
+              <Link to="/register">Please Register</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,21 +1,33 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import auth from "../../firebase/firebase.config";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [registerError, setRegisterError] = useState("");
   const [success, setSuccess] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(email, password);
+    const accepted = e.target.terms.checked;
+    console.log(email, password, accepted);
 
     setRegisterError("");
     setSuccess("");
     if (password.length < 6) {
       setRegisterError("Password should be at least 6 characters or longer");
+      return;
+    } else if (!/[A-Z]/.test(password)) {
+      setRegisterError(
+        "Your password should have at least one uppercase characters."
+      );
+      return;
+    } else if (!accepted) {
+      setRegisterError("Please accept our terms and conditions!");
       return;
     }
 
@@ -36,7 +48,7 @@ const Register = () => {
         <h2 className="text-3xl mb-8">Please Register</h2>
         <form onSubmit={handleRegister}>
           <input
-            className="mb-4 w-3/4 py-2 px-4"
+            className="mb-4  w-full py-2 px-4"
             placeholder="Email Address"
             type="email"
             name="email"
@@ -44,23 +56,41 @@ const Register = () => {
             required
           />
           <br />
-          <input
-            className="mb-4 w-3/4 py-2 px-4"
-            placeholder="Password"
-            type="password"
-            name="password"
-            required
-            id=""
-          />
+          <div className="relative mb-4">
+            <input
+              className=" w-full py-2 px-4"
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              required
+              id=""
+            />
+            <span
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute top-2 right-2 text-2xl"
+            >
+              {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+            </span>
+          </div>
+          <br />
+          <div className="mb-2">
+            <input type="checkbox" name="terms" id="terms" />
+            <label className="ml-2" htmlFor="terms">
+              Accept our <a href="">Terms and Conditions</a>
+            </label>
+          </div>
           <br />
           <input
-            className="mb-4 w-3/4 btn btn-secondary"
+            className="mb-4  w-full btn btn-secondary"
             type="submit"
             value="Register"
           />
         </form>
         {registerError && <p className="text-red-700">{registerError}</p>}
         {success && <p className="text-green-600">{success}</p>}
+        <p>
+          Already have an account ? <Link to="/login">Login</Link>
+        </p>
       </div>
     </div>
   );
